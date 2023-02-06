a30593cdc73ae9ce1485a479ca98707fd02fb1ab
Helar Kärbo <helar.karbo@gmail.com>
2023-02-06 13:14:09 +0200

    Toytrade backend initial.

---
+++ src/main/java/ee/valiit/back_toytrade/BackToytradeApplication.java
@@ -0,0 +1,13 @@
+package ee.valiit.back_toytrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackToytradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackToytradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/valiit/back_toytrade/BackToytradeApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.valiit.back_toytrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackToytradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
